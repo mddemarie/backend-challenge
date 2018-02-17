201ee43f1ecdde330f4c65befa34d01d6793b0c4
mddemarie <mddemarie@gmail.com>
2018-02-17 20:32:59 +0100

    added clearer User model

---
+++ api/models.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app import app, db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(32), index=True, unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(128))
