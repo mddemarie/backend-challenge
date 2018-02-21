bfec9e892ba7918bfd834b6dd67b8aa9ffc2dd4b
mddemarie <mddemarie@gmail.com>
2018-02-21 12:00:12 +0100

    improved email & password forms

--- api/forms.py
+++ api/forms.py
@@ -1,6 +1,7 @@
-from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
-    email = TextField('email')
-    password = PasswordField('password')
+class EmailPasswordForm(Form):
+    email = StringField('Email', validators=[DataRequired])
+    password = PasswordField('Password', validators=[DataRequired])
