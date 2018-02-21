7abc71741c79072d8a658892b991d59fca29a5b5
mddemarie <mddemarie@gmail.com>
2018-02-21 12:02:57 +0100

    changed auth.email to auth.username, auth.email does not exist

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, request, jsonify, make_response
-from app import app, db
-import uuid, jwt, datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+import uuid, jwt, datetime
 from functools import wraps
+
+from app import app
 from api.models import User
 
 def token_required(f):
@@ -90,13 +91,12 @@ def delete_user(current_user, public_id):
 @app.route('/login')
 def login():
     auth = request.authorization
-    if not auth or not auth.email or not auth.password:
+    if not auth or not auth.username or not auth.password:
         return make_response('Could not verify.', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
-    user = User.query.filter_by(email=auth.email).first()
+    user = User.query.filter_by(email=auth.username).first()
     if not user:
         return make_response('Could not verify.', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
     if check_password_hash(user.password, auth.password):
         token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
         return jsonify({'token': token.decode('UTF-8')})
-    return make_response('Could not verify.', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
-    
+    return make_response('Could not verify.', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})  
